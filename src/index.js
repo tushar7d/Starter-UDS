3309cb81a3e39d67e358c9ec1114ffe1a66bf182
Tushar <tdebnath@expedia.com>
2019-10-20 14:16:20 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css'
-class App extends React.Component {
-  render() {
+const App =() =>{
+  
     return (
       <React.Fragment>
         <h2>Hello!</h2>
       </React.Fragment>
     );
-  }
 }
 
 const rootElement = document.getElementById("root");
