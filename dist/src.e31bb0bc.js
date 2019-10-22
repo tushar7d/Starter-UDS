e1bcac2d004f68be6d8b75b81bb67ff8a294680f
Tushar <tdebnath@expedia.com>
2019-10-23 00:27:27 +0530

    changes

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -37904,7 +37904,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54953" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60580" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
