253ef45411bed99a7825a089a63f9ffcb444e5a4
Tushar <tdebnath@expedia.com>
2019-10-24 12:33:09 +0530

    changes

--- src/Pages/playground.js
+++ src/Pages/playground.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import { Stack, Box, Text, Icon } from "../components";
-import styled from 'styled-components'
+import styled from "styled-components";
 
-let StyledEditor= styled(LiveEditor)`
-  width: 100vw;
-  height: 40vh;
+let StyledEditor = styled(LiveEditor)`
+  width: 50vw;
+  height: 100vh;
   background-color: #000000;
   caret-color: red;
-`
+`;
 
 let StyledPre = styled(LivePreview)`
-width: 100vw;
-height: 60vh;
-`
-const scope = {Stack, Box, Text, Icon };
+  width: 50vw;
+  height: 80vh;
+`;
+
+let StyledEror = styled(LiveError)`
+  background-color: midnightblue;
+  color: white;
+  height: 20vh;
+  width: 50vw;
+  margin: 0;
+`;
+const scope = { Stack, Box, Text, Icon };
 
 let code = `<Box size="100%" bg="background.1">
   <Stack height="100%" horizontal center mid>
@@ -24,14 +32,20 @@ let code = `<Box size="100%" bg="background.1">
     <Box size="100px" bg="accent.2" m="2"/>
   </Stack>
 </Box>
-`
+`.trim();
 
 const Playground = () => {
   return (
     <LiveProvider scope={scope} code={code}>
-      <StyledPre />
-      <LiveError />
-      <StyledEditor  padding={10}/>
+      <Stack width="100vw" height="100vh" horizontal top split>
+        <Stack>
+          <StyledEditor />
+        </Stack>
+        <Stack>
+          <StyledPre />
+          <StyledEror />
+        </Stack>
+      </Stack>
     </LiveProvider>
   );
 };
